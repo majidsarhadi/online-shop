a00fb1c0181ee6252144d99519c0710e31e61174
magid <magid@gmail.com>
2024-09-05 18:33:09 +0430

    add crispy_forms and bootstrap

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 # Create your tests here.
+from django.urls import reverse
+
+
+class SignUpAccountTes(TestCase):
+    def test_sign_up_by_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual( response.status_code, 200)
+
+    def test_sign_up_by_url(self):
+        response = self.client.get('/accounts/signup/')
+        self.assertEqual(response.status_code, 200)
+
+
